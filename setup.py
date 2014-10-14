2d161af3f6a6138fa444ac125009261d90e378b2
Bartosz Marcinkowski <bm371613@gmail.com>
2014-10-14 11:01:06 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='rich_iterator',
+    version='0.1',
+    author='Bartosz Marcinkowski',
+    author_email='bm371613@gmail.com',
+    url='https://github.com/bm371613/rich_iterator',
+    packages=['rich_iterator'],
+)
